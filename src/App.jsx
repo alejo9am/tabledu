d81f280b72f19dfbc095a81f7edb2e95d0447b54
Alejo Arenas Moreira <arenasmoreira@gmail.com>
2026-05-19 12:12:57 +0200

    chore(vercel): Configure vercel analytics and vercel speed insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'next-themes'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import { AuthProvider } from './context/AuthContext'
 import { Toaster } from './components/ui/sonner'
 import AppRoutes from './router/AppRoutes'
@@ -11,6 +13,8 @@ function App() {
         <AuthProvider>
           <AppRoutes />
           <Toaster position="top-center" />
+          <Analytics />
+          <SpeedInsights />
         </AuthProvider>
       </ThemeProvider>
     </BrowserRouter>
